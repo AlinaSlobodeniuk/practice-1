3559481f0f2065df84e59e9f3ee204a48be5d41a
Alina Slobodeniuk <112395038+AlinaSlobodeniuk@users.noreply.github.com>
2025-03-16 12:34:48 +0200

    2

--- js/lesson-4.js
+++ js/lesson-4.js
@@ -1,15 +1,56 @@
+// Завдання 1
+
 // 1 - отримай body елемент і виведи його в консоль;
+const body = document.querySelector("body");
+console.log(body);
+
 // 2 - отримай елемент id="title" і виведи його в консоль;
+const title = document.querySelector("#title");
+console.log(title);
+
 // 3 - отримай елемент class="list" і виведи його в консоль;
+const list = document.querySelector(".list");
+console.log(list);
+
 // 4 - отримай всі елементи з атрибутом data-topic і виведи їх в консоль;
+const topic = document.querySelectorAll('[data-topic]');
+console.log(topic);
+
 // 5 - отримай перший елемент з списку всіх елементів з атрибутом data-topic і виведи його в консоль;
+const firstTopic = document.querySelectorAll('[data-topic]');
+console.log(firstTopic[0]);
+
 // 6 - отримай останній елемент з списку всіх елементів з атрибутом data-topic і виведи його в консоль;
+const lastTopic = document.querySelectorAll('[data-topic]');
+const topicLength = firstTopic.length - 1;
+console.log(lastTopic[topicLength]);
+
+
+
 // 7 - який елемент є сусідом для h1? Знайти і виведи його в консоль;
-// 8 - по тегу h3 знайти всі заголовки та виведи їх у консоль;
+const element = document.querySelector("h1");
+console.log(element.nextElementSibling);
+
+// 8 - по тегу h3 знайти всі заголовки та вивед їх у консоль;
+const elementTitle = document.querySelectorAll("h3");
+elementTitle.forEach(element => {
+     console.log(element.textContent);
+});
+
 // 9 - для кожного елмента h3 додай class="active", який змінить колір заголовка на червоний колір
+// elementTitle.classList.add("active");
+elementTitle.forEach(element => {
+    element.classList.add("active");    
+});
+
 // 10 - знайти елемент li який має атрибут data-topic з значенням "navigation" і виведи його в консоль;
+const findLi = document.querySelector('li[data-topic = "navigation"]');
+console.log(findLi);
+
 // 11 - додай для знайденого елемента data-topic="navigation" атрибут style і зроби його backgroundColor жовтим
+findLi.style.backgroundColor = "yellow";
 // 12 - у елемента data-topic="navigation" знайди елемент р і зміни його текст на "Я змінив тут текст!".
+
 // 13 - створи const currentTopic = "manipulation"; після цього знайди елемент у якогоо атрибут data-topic має значення, яке зберігається у змінній currentTopic і виведи його в консоль;
 // 14 - додай до знайденого елемента атрибут style і зроби його backgroundColor блакитним;
 // 15 - знайти в документі заголовок, який має class="completed" і виведи його в консоль;
@@ -19,6 +60,71 @@
 // 19 - зроби це саме, але використовуй шаблонні рядки та метод insertAdjacentHTML()
 // 20 - очисти список
 
+// Завдання 2
 
-const body = document.querySelector("body");
-console.log(body);
+// Створіть контейнер div (з класом numberContainer )в HTML-документі
+// та динамічно створіть 100 блоків (з класом number) наповнивши їх рандомними
+// числами від 1 до 100 і додайте їх до контейнера div(numberContainer).
+// Парні числа повинні мати зелений фон (додати клас even),
+// Непарні числа - жовтий фон (додати клас odd).
+
+// const randomNumber = () => Math.floor(Math.random() * 100) + 1;
+
+
+
+
+
+
+// Завдання 3
+
+// Form Events, Input, Focus, Blur and Submit.
+
+// Використовуй шаблон форми з файлу html.
+
+// 1 - При події `input`, якщо користувач ввів в поле більше
+// 6 символів то додати клас `success`. Якщо ж символів менше аніж 6,
+// то клас `error`
+
+// 2 - При події `focus` зроби перевірку на пустоту поля інпута,
+// якщо ж поле пусте, то зроби `outline` => `'3px solid red'`,
+// якщо при фокусі поле непусте, то `outline` => `'3px solid green'`
+
+// 3 - При події `blur` зроби перевірку на пустоту поля інпута,
+// якщо ж поле пусте, то зроби `outline` => `'3px solid red'`,
+// якщо при фокусі поле непусте, то `outline` => `'3px solid lime'`
+
+// 4 - При події `submit`. Відміни поведінку браузера по змовчуванню.
+// Дістань данні з інпуту і чек боксу, зроби перевірку,
+// що інпут не порожній, також, що нажатий чек бокс у положення true,
+// якщо користувач все виконав вірно, збери данні (userName)
+// у обьект і виведи у консоль. У разі, якщо користувач не виконав
+// одну із умов, виведи повідомлення. Також реалізуй додавання
+// ім`я користувача у span, замість слова "Anonymous".
+// Якщо користувач ввів ім`я, а потім видали, зроби так,
+// щоб на місце повернулось дефолтне знаяення "Anonymous".
+// При відправці форми, очисти інпут, верни чек бокс у положення
+// false, верни дефолтне значення "Anonymous" у span.
+
+
+
+
+
+
+// Завдання 4
+
+ // Використовуй шаблон розмітки з файлу html та напиши наступний функціонал:
+ // При кліку на кнопку "Зменшити" квадрат стає меньшим на 20 пікселів, 
+ // При кліку на кнопку "Збільшити" - квадрат стає більшим на 20 пікселів.
+ 
+ 
+ 
+ 
+
+
+
+
+
+
+
+
+
